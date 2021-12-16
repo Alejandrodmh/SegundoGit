0216d8ace84ff922ff4c0880dac0d48a623142c6
Alejandrodmh <admoralh01@iesarroyoharnina.es>
2021-12-16 13:20:13 +0100

    pruebas1.0

--- src/LaOca/Oca.java
+++ src/LaOca/Oca.java
@@ -2,28 +2,48 @@ package LaOca;
 
 import java.util.Random;
 
-
 public class Oca {
 
 	public static void main(String[] args) {
 		Casilla tablero[];
-		tablero=new Casilla[63];
+		tablero = new Casilla[63];
 		for (int i = 0; i < tablero.length; i++) {
-			tablero[i]=Casilla.blanco;
+			tablero[i] = Casilla.blanco;
 			System.out.println(tablero[i]);
 		}
 	}
-	/**
-	 * cambia la naturaleza de la casilla 19( y solo esa)
-	 * que pasa a ser posada
-	 * @param tablero
-	 */
-	public static void colocarPosada(Casilla[] tablero) {}
-	/**
-	 * Cambia la naturaleza de la casilla 31 (solo la 31)
-	 *  que pasa de blanco a pozo
-	 * @param tablero
-	 */
-	public static void colocarPozo(Casilla[] tablero) {}
 
+	/**
+	 * cambia la naturaleza de la casilla 19( y solo esa) que pasa a ser posada
+	 * 
+	 * @param tablero
+	 */
+	public static void colocarPosada(Casilla[] tablero) {
+		tablero[19 - 1] = Casilla.posada;
+	}
+
+	/**
+	 * Cambia la naturaleza de la casilla 31 (solo la 31) que pasa de blanco a pozo
+	 * 
+	 * @param tablero
+	 */
+	public static void colocarPozo(Casilla[] tablero) {
+		tablero[31 - 1] = Casilla.pozo;
+	}
+
+	public static void colocarPuente(Casilla[] tablero) {
+		for (int i = 0; i < tablero.length; i++) {
+			if (i == 12 || i == 23) {
+				tablero[i - 1] = Casilla.puente;
+			}
+		}
+	}
+
+	public static void colocarOca(Casilla[] tablero) {
+		for (int i = 0; i < tablero.length; i++) {
+			if (i % 9 == 0) {
+				tablero[i - 1] = Casilla.oca;
+			}
+		}
+	}
 }
--- src/LaOca/OcaTest2.java
+++ src/LaOca/OcaTest2.java
@@ -1,12 +1,11 @@
 package LaOca;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class OCATest {
+class OcaTest2 {
 	Casilla tablero[];
 
 	@BeforeEach
@@ -18,7 +17,8 @@ class OCATest {
 	}
 
 	@Test
-	void testPosada() {
+	void testColocarPosada() {
+
 		Oca.colocarPosada(tablero);
 		int posada = 19;
 		comprobarPosicion(Casilla.posada, posada);
@@ -30,23 +30,25 @@ class OCATest {
 		int pozo = 31;
 		comprobarPosicion(Casilla.pozo, pozo);
 	}
+
 	@Test
 	void testColocarPuente() {
 		Oca.colocarPuente(tablero);
-		int puente1=12;
-		int puente2=23;
-		comprobarPosicion(Casilla.puente,puente1);
-		comprobarPosicion(Casilla.puente,puente2);
+		int puente1 = 12;
+		int puente2 = 23;
+		comprobarPosicion(Casilla.puente, puente1);
+		comprobarPosicion(Casilla.puente, puente2);
 	}
+
 	@Test
 	void testColocarOca() {
 		Oca.colocarOca(tablero);
 		int oca = 0;
-		for (int i = 0; i < 64; i++) {
-			if(i%9==0) {
-				oca=i;
+		for (int i = 0; i < 63; i++) {
+			if (i % 9 == 0) {
+				oca = i;
 			}
-			comprobarPosicion(Casilla.oca,oca);
+			comprobarPosicion(Casilla.oca, oca);
 		}
 	}
 
@@ -58,6 +60,6 @@ class OCATest {
 				assertEquals(tipo, tablero[posicion - 1]);
 			}
 		}
-	}
 
+	}
 }
